205b16f830531f61fac7401b6393a126f6af1276
Jean Karel <mailjeank@gmail.com>
2023-03-25 15:50:25 -0500

    github integration

---
+++ cookery/Views/cookeryApp.swift
@@ -0,0 +1,17 @@
+//
+//  cookeryApp.swift
+//  cookery
+//
+//  Created by Jean Karel on 25/03/23.
+//
+
+import SwiftUI
+
+@main
+struct cookeryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
