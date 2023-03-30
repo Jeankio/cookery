5221c915d3dcb96c20689bcf683760f0cbfc0272
Jean Karel <mailjeank@gmail.com>
2023-03-30 18:56:41 -0500

    Avances en el proyecto

---
+++ cookery/Views/RecipeCategoryView.swift
@@ -0,0 +1,20 @@
+//
+//  RecipeCategoryView.swift
+//  cookery
+//
+//  Created by Jean Karel on 30/03/23.
+//
+
+import SwiftUI
+
+struct RecipeCategoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecipeCategoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeCategoryView()
+    }
+}
---
+++ cookery/Views/RecipeFoodTypeView.swift
@@ -0,0 +1,20 @@
+//
+//  RecipeFoodTypeView.swift
+//  cookery
+//
+//  Created by Jean Karel on 30/03/23.
+//
+
+import SwiftUI
+
+struct RecipeFoodTypeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecipeFoodTypeView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeFoodTypeView()
+    }
+}
